f3f7a344f15e5c5e7b07766cd2475a644fd653a3
souravkairy <48748661+souravkairy@users.noreply.github.com>
2021-08-11 12:22:28 +0600

    new

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::get('/active-users', 'App\Http\Controllers\backEnd\Admin\UserListControll
 
 Route::get('/pending-withdraw-list', 'App\Http\Controllers\backEnd\Admin\WithdrawController@pending_withdraw');
 Route::get('/completed-withdraw-list', 'App\Http\Controllers\backEnd\Admin\WithdrawController@completed_withdraw');
+Route::get('/completed-withdt', 'App\Http\Controllers\backEnd\Admin\WithdrawController@completed_withdraw');
 
 
 //user section route------------------------------------------
